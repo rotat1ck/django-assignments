36ee91fe1b370e31956a5e1b81a09ab931fb64b9
rotat1ck <rotatick32151@gmail.com>
2025-01-23 14:30:18 +0300

    23.01 classwork

--- projects/test/app/views.py
+++ projects/test/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello_world(request):
+    return HttpResponse("Hello, world!")
+
+def home(request):
+    return render(request, 'home.html')
